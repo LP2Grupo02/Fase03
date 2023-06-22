fc813d7e62f108566ea1a7052bf9cd9467e2b258
LP2Grupo02 <131925876+LP2Grupo02@users.noreply.github.com>
2023-06-21 20:54:35 -0500

    Update G10_Ejercicio03.cpp

--- G10_Ejercicio03.cpp
+++ G10_Ejercicio03.cpp
@@ -1,48 +1,68 @@
 #include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <boost/archive/binary_oarchive.hpp> 
+#include <boost/archive/binary_iarchive.hpp> 
+ 
 using namespace std; 
  
 class Persona { 
-private: 
+public: 
     string nombre; 
     int edad; 
     string direccion; 
  
-public: 
+    Persona(const string& _nombre = "", int _edad = 0, const string& _direccion = "") 
+        : nombre(_nombre), edad(_edad), direccion(_direccion) {} 
  
-    Persona(string _nombre, int _edad, string _direccion) { 
+    void setNombre(const string& _nombre) { 
         nombre = _nombre; 
-        edad = _edad; 
-        direccion = _direccion; 
     } 
  
-
-    void setNombre(string _nombre) { 
-        nombre = _nombre; 
+    string getNombre() const { 
+        return nombre; 
     } 
  
     void setEdad(int _edad) { 
         edad = _edad; 
     } 
  
-    void setDireccion(string _direccion) { 
-        direccion = _direccion; 
-    } 
- 
-    string getNombre() { 
-        return nombre; 
-    } 
- 
-    int getEdad() { 
+    int getEdad() const { 
         return edad; 
     } 
  
-    string getDireccion() { 
+    void setDireccion(const string& _direccion) { 
+        direccion = _direccion; 
+    } 
+ 
+    string getDireccion() const { 
         return direccion; 
+    } 
+ 
+    template <class Archive> 
+    void serialize(Archive& ar, const unsigned int version) { 
+        ar & nombre; 
+        ar & edad; 
+        ar & direccion; 
     } 
 }; 
  
 int main() { 
-     
+    Persona persona("Raul Romero", 20, "Av Mariscal Castilla 200"); 
  
-    return 0; 
-} 
+    ofstream archivo("persona.bin", ios::binary); 
+    boost::archive::binary_oarchive salidaArchivo(archivo); 
+    salidaArchivo << persona; 
+    archivo.close(); 
+ 
+    ifstream archivoEntrada("persona.bin", ios::binary); 
+    boost::archive::binary_iarchive entradaArchivo(archivoEntrada); 
+    Persona personaDeserializada; 
+    entradaArchivo >> personaDeserializada; 
+    archivoEntrada.close(); 
+ 
+    cout << "Nombre: " << personaDeserializada.getNombre() << endl; 
+    cout << "Edad: " << personaDeserializada.getEdad() << endl; 
+    cout << "Dirección: " << personaDeserializada.getDireccion() << endl; 
+ 
+    return 0;
