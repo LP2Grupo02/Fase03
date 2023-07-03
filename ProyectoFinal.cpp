3f6d2cd29ad30d3e8a7b5014b71758c1ec171ba8
LP2Grupo02 <131925876+LP2Grupo02@users.noreply.github.com>
2023-07-03 11:50:43 -0500

    Update ProyectoFinal.cpp

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <vector>
 #include <fstream>
 
@@ -120,63 +119,6 @@ class Productos {
 vector<Productos> veProductos;
 vector<Clientescorporativos> cliCO;
 vector<Clientesindividuales> cliIN;
-
-void reporteVentas() {
-    string codigo;
-    char tipo;
-    cout << "              Reporte de ventas por Clientes             " << endl;
-    cout << "Ingrese el tipo de cliente a buscar (I/C): ";
-    cin >> tipo;
-    cout << "Ingrese el código del cliente: ";
-    cin >> codigo;
-
-    if (tipo == 'I') {
-        bool encontrado = false;
-        for (const auto& cliente : cliIN) {
-            if (cliente.codigo == codigo) {
-                cout << "Cliente encontrado"<< endl;
-
-                for (Productos& producto : veProductos) {
-                    cout<<"                                Venta 001                               "<<endl;
-                    cout<<"Fecha 27/06/2023"<<endl;
-                    cout<<"COD PRODUCTO             CANTIDAD            PRECIO              SUBTOTAL"<<endl;
-                    cout<<producto.codigo<<" "<<producto.nombre<<"              "<<producto.precio<<"               "<<producto.stock<<"                    "<<producto.precio*producto.stock<<endl;
-                    cout << "--------------------------------------------------------------------------------------" << endl;
-                }
-                encontrado = true;
-                break;
-            }
-        }
-        if (!encontrado) {
-            cout << "Cliente no encontrado" << endl;
-        }
-    }
-    else if (tipo == 'C') {
-        bool encontrado = false;
-        for (const auto& cliente : cliCO) {
-            if (cliente.codigo == codigo) {
-                cout << "Cliente encontrado"<< endl;
-                
-                for (Productos& producto : veProductos) {
-                    cout<<"                                Venta 001                               "<<endl;
-                    cout<<"Fecha 27/06/2023"<<endl;
-                    cout<<"COD PRODUCTO             CANTIDAD            PRECIO              SUBTOTAL"<<endl;
-                    cout<<producto.codigo<<" "<<producto.nombre<<"              "<<producto.precio<<"               "<<producto.stock<<"                    "<<producto.precio*producto.stock<<endl;
-                    cout << "--------------------------------------------------------------------------------------" << endl;
-                }
-                encontrado = true;
-                break;
-            }
-        }
-    
-        if (!encontrado) {
-            cout << "Cliente no encontrado" << endl;
-        }
-    }
-    else {
-        cout << "Tipo de cliente inválido" << endl;
-    }
-}
 
 void NuevosClientes() {
     string correo = "xyz", codigo = "004", nombre = "", telefono = "123", direccion = "", ruc = "321", dni = "123", categoria = "Y";
@@ -551,6 +493,15 @@ void MostrarProductos() {
         cout << endl;
     }
 }
+vector<Productos> buscarProductosPorCodigo(const string& codigo) {
+    vector<Productos> productosCompra;
+    for (auto& producto : veProductos) {
+        if (producto.codigo == codigo) {
+            productosCompra.push_back(producto);
+        }
+    }
+    return productosCompra;
+}
 
 void menu() {
     string opc = "0";
@@ -592,6 +543,37 @@ void menu() {
             system("cls");
         } else if (opc == "5") { 
             cout << "Selecciono VENTAS" << endl;
+            cout << "Selecciono VENTAS" << endl;
+            string codigoBuscado;
+            float cantidadCompra, subtotal;
+            bool continuar = true;
+            while (continuar) {
+            cout<<"Ingrese el codigo del producto"<<endl;
+            cin>>codigoBuscado;
+            vector<Productos> productosCompra = buscarProductosPorCodigo(codigoBuscado);
+            if (!productosCompra.empty()) {
+            cout << "Producto a comprar encontrado:" << endl;
+            cout << "Ingrese la cantidad"<<endl;
+            cin>>cantidadCompra;
+            for (auto& producto : productosCompra) {
+            cout << "Ingrese la cantidad"<<endl;
+            cout<<producto.precio;
+            subtotal=cantidadCompra*producto.precio;
+                    cout<<"                                Venta                             "<<endl;
+                    cout<<"Fecha 27/06/2023"<<endl;
+                    cout<<"COD PRODUCTO                   CANTIDAD            PRECIO              SUBTOTAL"<<endl;
+                    cout<<producto.codigo<<" "<<producto.nombre<<"                  "<<cantidadCompra<<"                   "<<producto.precio<<"                    "<<subtotal<<endl;
+                    cout << "--------------------------------------------------------------------------------------" << endl;
+            }
+            } else {
+                cout << "No se encontraron productos con el código especificado." << endl;
+            }
+            char opcion;
+            cout << "¿Desea realizar otra compra? (s/n): "<<endl;
+            cin >> opcion;
+            if (opcion != 's' && opcion != 'S') {
+            continuar = false;
+            }}
             system("pause");
             system("cls");
         } else if (opc == "6") { 
